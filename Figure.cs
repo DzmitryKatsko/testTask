e504bec6ea3a25161aeec65f4e87ba55d0e4672f
DzmitryKatsko <108495954+DzmitryKatsko@users.noreply.github.com>
2023-04-10 21:43:28 +0200

    Add files via upload

---
+++ Figure.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace AreaLibrary
+{
+    public interface IFigure
+    {
+        double CalculateArea();
+    }
+
+    public class Circle : IFigure
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    public class Triangle : IFigure
+    {
+        private double side1;
+        private double side2;
+        private double side3;
+
+        public Triangle(double side1, double side2, double side3)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+            this.side3 = side3;
+        }
+
+        public double CalculateArea()
+        {
+            double p = (side1 + side2 + side3) / 2;
+            return Math.Sqrt(p * (p - side1) * (p - side2) * (p - side3));
+        }
+
+        public bool IsRightTriangle()
+        {
+            double[] sides = { side1, side2, side3 };
+            Array.Sort(sides);
+            return sides[2] * sides[2] == sides[0] * sides[0] + sides[1] * sides[1];
+        }
+    }
+
+    public class FigureCalculator
+    {
+        public static double CalculateArea(IFigure figure)
+        {
+            return figure.CalculateArea();
+        }
+    }
+}
+        
---
+++ Tests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using AreaLibrary;
+using NUnit.Framework;
+
+namespace AreaTest
+{
+
+    [TestClass]
+    public class AreaCalculatorTests
+    {
+        [TestMethod]
+        public void CircleAreaTest()
+        {
+            double radius = 5;
+            Circle circle = new Circle(radius);
+            
+            double area = circle.CalculateArea();
+            
+            Assert.AreEqual(Math.PI * radius * radius, area);
+        }
+
+        [TestMethod]
+        public void TriangleAreaTest()
+        {
+            // Arrange
+            double side1 = 3;
+            double side2 = 4;
+            double side3 = 5;
+            Triangle triangle = new Triangle(side1, side2, side3);
+
+            // Act
+            double area = triangle.CalculateArea();
+
+            // Assert
+            Assert.AreEqual(6, area);
+        }
+
+        [TestMethod]
+        public void RightTriangleTest()
+        {
+            // Arrange
+            double side1 = 3;
+            double side2 = 4;
+            double side3 = 5;
+            Triangle triangle = new Triangle(side1, side2, side3);
+            
+            bool isRight = triangle.IsRightTriangle();
+            
+            Assert.IsTrue(isRight);
+        }
+    }
+
+}
